4580233b92ff5d2ff172bed29a21b4a3cb33d0d0
jsfernando <jsfernando@gmail.com.br>
2022-11-12 18:40:13 -0300

    my first commit after expo: ~44.0.2, bug fix react-native: 0.64.4,

--- ios/myshopping/AppDelegate.m
+++ ios/myshopping/AppDelegate.m
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -29,6 +29,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+[FIRApp configure];
 #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
   InitializeFlipper(application);
 #endif
