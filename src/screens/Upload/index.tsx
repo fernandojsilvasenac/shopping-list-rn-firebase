546ce1cdeb6a17e6f0f5c14ad363c6a9f4651281
Fernando Jacinto da Silva <fernando.jsilva@sp.senac.br>
2022-11-23 21:10:03 -0300

    added react-native-firebase/firestore, screens/upload

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import storage from '@react-native-firebase/storage';
+
 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Photo } from '../../components/Photo';
 
 import { Container, Content, Progress, Transferred } from './styles';
+import { Alert } from 'react-native';
 
 export function Upload() {
   const [image, setImage] = useState('');
+  //2
+  const [bytesTransferred, setBytesTransferred] = useState('');
+  const [progress, setProgress] = useState('0');
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -26,24 +32,60 @@ export function Upload() {
     }
   };
 
+  async function handleUpload(){
+    // criou um nome para o arquivo
+    const fileName = new Date().getTime();
+    //4
+    //deixar o nome do arquivo e extensão dinâmica
+    const MIME = image.match(/\.(?:.(?!\.))+$/);
+    // const reference = storage().ref(`/images/${fileName}${MIME}`);
+
+    // pegou a referencia onde o arquivo vai ser salvo
+    const reference = storage().ref(`/images/${fileName}.png`);
+
+    //1ª
+    // reference
+    // .putFile(image)
+    // .then( () => Alert.alert('Upload concluído!!!'))
+    // .catch( (error) => console.log(error));
+
+    //3
+    const uploadTask = reference.putFile(image)
+    // escutando as mudanças do upload on.state_changed...
+    uploadTask.on('state_changed', taskSnapshot => {
+      // percentual de total de bytes transferidos / por total de baytes que contem o arquivo /dividido por 100, vai dar o percentual
+      const percent = ((taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) *100).toFixed(0);
+      // atualizando o estado e mostrando abaixo 
+      setProgress(percent);
+      // atualizando o texto abaixo da imagem
+      setBytesTransferred(`${taskSnapshot.bytesTransferred} transferido de ${taskSnapshot.totalBytes}`)
+
+    })
+    //quando terminar o upload, informar ao usuario
+    uploadTask.then( () => {
+      Alert.alert('Upload concluído com sucesso!!!')
+    })
+
+  }
+
   return (
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Uploads de Fotos" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
 
         <Button
           title="Fazer upload"
-          onPress={() => { }}
+          onPress={handleUpload}
         />
 
         <Progress>
-          0%
+          {progress}%
         </Progress>
 
         <Transferred>
-          0 de 100 bytes transferido
+          {bytesTransferred}
         </Transferred>
       </Content>
     </Container>
